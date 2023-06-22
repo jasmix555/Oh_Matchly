f5fd4af9a04a2254aadb90d25bb9a99599158799
mk <kanzakimiki.12@gmail.com>
2023-06-22 16:38:32 +0900

    shuffle

--- public/js/shuffle.js
+++ public/js/shuffle.js
@@ -1,18 +1,49 @@
-console.log("hello");
+const body = document.querySelector("body");
+const img = document.querySelector(".machine");
+const move = document.querySelector(".move");
+const move02 = document.querySelector(".move02");
 
-const img = document.querySelector(`img`);
-const moveBox = `img/miki/shuffle_box_move.png`;
+const capsule = document.querySelector(`.capsule3`);
+const modal = document.querySelector(".modal");
+const ball = document.querySelector(".ball");
 
-const cap = ["", "", ""];
+const random = document.querySelector(".random");
+
+const purple = document.querySelector(".purple");
+
+const close = document.querySelector(".fa-xmark");
 
 const btn = document.querySelector("button");
-console.log(btn);
-
-const interval = 300;
 
 btn.addEventListener("click", (e) => {
-  console.log("クリック");
   btn.remove();
 
-  img.src = moveBox;
+  move.classList.toggle("active");
+  move02.classList.toggle("active");
+  const active = document.querySelector(`.active`);
+  console.log(active);
+  move02.addEventListener("animationend", () => {
+    capsule.classList.toggle("active");
+    modal.classList.toggle("active");
+  });
+  modal.addEventListener("animationend", () => {
+    ball.classList.toggle("active");
+    move02.classList.toggle("active");
+    move.classList.toggle("active");
+  });
+  purple.addEventListener("click", () => {
+    // random.classList.toggle("active");
+    toggle(random, "active");
+    ball.remove();
+    capsule.remove();
+  });
+  close.addEventListener("click", () => {
+    toggle(random, "active");
+    ball.add();
+    capsule.add();
+  });
 });
+
+function toggle(elem, className) {
+  elem.classList.toggle(className);
+}
