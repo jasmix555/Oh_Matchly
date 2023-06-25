0a5e8573aae253b8068c87900b6de671ff93d6df
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-06-26 00:56:24 +0900

    create login page

---
+++ public/js/password.js
@@ -0,0 +1,11 @@
+const togglePassword = document.querySelector("#toggle_password");
+const password = document.querySelector("#text_password");
+
+togglePassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  // toggle the eye slash icon
+  this.classList.toggle("fa-eye");
+});
--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,52 +1,4 @@
 {
-  // document.addEventListener("DOMContentLoaded", function () {
-  //   var nopeButton = document.querySelector(".but-nope");
-  //   var yayButton = document.querySelector(".but-yay");
-  //   var cards = Array.from(document.querySelectorAll(".card"));
-  //   var currentIndex = 0;
-  //   nopeButton.addEventListener("click", function () {
-  //     var currentCard = document.querySelector(".current");
-  //     var nextCard = currentCard.nextElementSibling;
-  //     if (nextCard !== null) {
-  //       currentCard.classList.remove("current");
-  //       currentCard.classList.add("rotate-left");
-  //       nextCard.classList.add("current");
-  //     } else {
-  //       currentCard.classList.remove("current");
-  //       currentCard.classList.add("rotate-left");
-  //       currentIndex = (currentIndex + 1) % cards.length;
-  //       cards[currentIndex].classList.add("current");
-  //       if (currentIndex === 0) {
-  //         resetRotationClasses();
-  //       }
-  //     }
-  //   });
-  //   yayButton.addEventListener("click", function () {
-  //     var currentCard = document.querySelector(".current");
-  //     var nextCard = currentCard.nextElementSibling;
-  //     if (nextCard !== null) {
-  //       currentCard.classList.remove("current");
-  //       currentCard.classList.add("rotate-right");
-  //       nextCard.classList.add("current");
-  //     } else {
-  //       currentCard.classList.remove("current");
-  //       currentCard.classList.add("rotate-right");
-  //       currentIndex = (currentIndex + 1) % cards.length;
-  //       cards[currentIndex].classList.add("current");
-  //       if (currentIndex === 0) {
-  //         resetRotationClasses();
-  //       }
-  //     }
-  //   });
-  //   function resetRotationClasses() {
-  //     cards.forEach(function (card) {
-  //       card.classList.remove("rotate-left");
-  //       card.classList.remove("rotate-right");
-  //     });
-  //   }
-  // });
-}
-{
   //counter function
   const counterElement = document.querySelector(".counter");
   const yayButton = document.querySelector("#love");
@@ -215,7 +167,7 @@
 
       clickCounter++;
 
-      if (clickCounter >= 10) {
+      if (clickCounter >= 5) {
         clickCounter = 0;
         cards.forEach(function (card) {
           card.classList.remove("removed");
