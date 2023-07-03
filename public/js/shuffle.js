33f31ab6bcef90f7c8c20b72b903a81119027cc6
mk <kanzakimiki.12@gmail.com>
2023-07-03 16:42:11 +0900

    reload

--- public/js/shuffle.js
+++ public/js/shuffle.js
@@ -8,10 +8,18 @@ const modal = document.querySelector(".modal");
 const ball = document.querySelector(".ball");
 
 const random = document.querySelector(".random");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+
+const capsules = document.querySelectorAll(".capsule");
+console.log(capsules);
 
 const purple = document.querySelector(".purple");
+const yellow = document.querySelector(".yellow");
+const red = document.querySelector(".red");
 
-const close = document.querySelector(".fa-xmark");
+const close = document.querySelectorAll(".fa-xmark");
 
 const btn = document.querySelector("button");
 
@@ -31,19 +39,75 @@ btn.addEventListener("click", (e) => {
     move02.classList.toggle("active");
     move.classList.toggle("active");
   });
-  purple.addEventListener("click", () => {
-    // random.classList.toggle("active");
-    toggle(random, "active");
-    ball.classList.toggle("active");
-    capsule.classList.toggle("active");
+  // capsules.forEach((e) => {
+  //   console.log(e);
+
+  // });
+
+  //動かん
+  // function toggle(elem, className) {
+  //   elem.classList.toggle(className);
+  // }
+
+  // for (let i = 0; i < capsules.length; i++) {
+  //   capsules[i].addEventListener("click", () => {
+  //     if (i === 0) {
+  //       console.log("one");
+  //       toggle(one, "active");
+  //       ball.classList.toggle("active");
+  //       capsule.classList.toggle("active");
+  //     } else if (i === 1) {
+  //       console.log("two");
+  //       toggle(two, "active");
+  //       ball.classList.toggle("active");
+  //       capsule.classList.toggle("active");
+  //     } else {
+  //       console.log("three");
+  //       toggle(three, "active");
+  //       ball.classList.toggle("active");
+  //       capsule.classList.toggle("active");
+  //     }
+  //   });
+  // }
+
+  // close.forEach((e) => {
+  //   e.addEventListener("click", () => {
+  //     console.log("削除");
+  //     toggle(random, "active");
+  //     ball.classList.toggle("active");
+  //     capsule.classList.toggle("active");
+  //   });
+  // });
+  console.log(close);
+
+  function toggle(elem, className) {
+    elem.classList.toggle(className);
+  }
+
+  for (let i = 0; i < capsules.length; i++) {
+    capsules[i].addEventListener("click", () => {
+      if (i === 0) {
+        console.log("one");
+        toggle(one, "active");
+      } else if (i === 1) {
+        console.log("two");
+        toggle(two, "active");
+      } else {
+        console.log("three");
+        toggle(three, "active");
+      }
+      toggle(ball, "active");
+      toggle(capsule, "active");
+    });
+  }
+
+  close.forEach((e, index) => {
+    e.addEventListener("click", () => {
+      console.log("削除");
+      toggle(random, "active");
+      toggle(ball, "active");
+      toggle(capsule, "active");
+      toggle(capsules[index], "active"); // Toggle the specific capsule element
+    });
   });
 });
-close.addEventListener("click", () => {
-  toggle(random, "active");
-  ball.classList.toggle("active");
-  capsule.classList.toggle("active");
-});
-
-function toggle(elem, className) {
-  elem.classList.toggle(className);
-}
